36658bb5e519b702e05268eef11de008a33bf028
Steven Huang <steven.charles.huang@gmail.com>
2018-10-13 00:25:54 -0400

    playing with string interpolation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,13 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
-import { Testcomponent1Component } from './testcomponent1/testcomponent1.component';
-import { Testcomponent2Component } from './testcomponent2/testcomponent2.component';
 
 @NgModule({
   // Components to be aware of at bundling
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent,
-    Testcomponent1Component,
-    Testcomponent2Component
+    ServersComponent
   ],
   imports: [
     BrowserModule,
